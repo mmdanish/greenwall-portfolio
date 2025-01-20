e07f80b54ed7c0ec2b5f4954e1c36a5fcf16ccbb
mmdanish <mhddanish567@gmail.com>
2025-01-20 19:25:22 +0530

    commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,18 +1,28 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
+  const [isScroll, setIsScroll] = useState(false);
+  const sideMenuRef = useRef();
 
-    const sideMenuRef = useRef();
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
 
-    const openMenu = () => {
-        sideMenuRef.current.style.transform = 'translateX(-16rem)'
-    }
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
 
-    const closeMenu = () => {
-        sideMenuRef.current.style.transform = 'translateX(16rem)'
-    }
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    });
+  }, []);
 
   return (
     <>
@@ -23,7 +33,11 @@ const Navbar = () => {
           className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]"
         />
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <nav
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
+          isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""
+        }`}
+      >
         <a href="">
           <Image
             src={assets.logo}
@@ -32,7 +46,11 @@ const Navbar = () => {
           />
         </a>
 
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+        <ul
+          className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
+            isScroll ? "" : "bg-white shadow-sm bg-opacity-50"
+          }`}
+        >
           <li>
             <a className="font-Ovo" href="#top">
               Home
@@ -78,11 +96,17 @@ const Navbar = () => {
 
         {/* ---------- mobile menu ------------ */}
 
-        <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
-
-            <div className="absolute right-6 top-6" onClick={closeMenu}>
-                <Image src={assets.close_black} alt="" className="w-5 cursor-pointer" />
-            </div>
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </div>
 
           <li>
             <a className="font-Ovo" onClick={closeMenu} href="#top">
