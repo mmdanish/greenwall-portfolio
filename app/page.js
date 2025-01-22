1df0fb1f58d10c01c17c386dcd762d37695f72e5
mmdanish <mhddanish567@gmail.com>
2025-01-22 22:03:23 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client"
+import { useEffect, useState } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -9,10 +10,32 @@ import Work from "./components/Work";
 
 
 export default function Home() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(()=>{
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setIsDarkMode(true)
+    } else {
+      setIsDarkMode(false)
+    }
+  },[])
+
+  useEffect(()=>{
+    if(isDarkMode) {
+      document.documentElement.classList.add('dark')
+      localStorage.theme = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark')
+      localStorage.theme = '';
+    }
+  }, [isDarkMode]);
+
+
   return (
     <>
-     <Navbar />
-     <Header />
+     <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+     <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
      <About />
      <Services />
      <Work />
